f8efec4d76f26defdc77f120b8b907efaf6473b8
VIVEKNIRALA <viveknirala84091@gmail.com>
2023-07-25 13:58:54 +0530

    version2.0 first commit

---
+++ src/pages/Courses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Courses = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      Comming Soon
+    </div>
+  )
+}
+
+export default Courses
